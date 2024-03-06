a53f0aa58d5ffbde401b300df25c7cf45a5cae0b
Babar404 <babarzaman7000@gmail.com>
2024-03-06 16:10:20 +1100

    Basic structure and CSV file read operation

---
+++ MyDataClass.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MyDataClass
+{
+    public class ActualResult
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using MyDataClass;
+
+namespace SuppliesPriceLister
+{
+    class Program
+    {
+        public static IConfiguration _config;
+        static void Main(string[] args)
+        {
+            _config = new ConfigurationBuilder()
+           .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+           .Build();
+
+            List<ActualResult> results = new List<ActualResult>();
+
+            ReadFromCSVFile(results);
+
+        }
+
+        private static void ReadFromCSVFile(List<ActualResult> actualResults)
+        {
+            string file = "..\\..\\..\\humphries.csv";
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(file))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] values = line.Split(',');
+                        if (values[0] != "identifier")
+                        {
+                            actualResults.Add(new ActualResult { Id = values[0], Name = values[1], Price = Math.Round(Double.Parse(values[3]), 2) });
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
